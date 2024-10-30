836cd72898ce7a1ba70c9cdb6cf771bb5403caf1
thfr <thfr.git@e.mail.de>
2024-10-30 23:48:53 +0100

    Add helpful comments

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# My 💤 LazyVim Based Config
+# My 💤 LazyVim Based NeoVim Config
+
+Based on the [💤 LazyVim Starter Template](https://github.com/LazyVim/starter/tree/7a10a75dc3fc2fc6a4f35f2eb33d12995ac9efca)
--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -1,18 +1,22 @@
+-- easymotion uses <Leader><Leader> as default which would hide the telescope shortcut
 vim.keymap.set("n", ",,", "<Plug>(easymotion-prefix)")
+
+-- spelling for english and german at the same time
 vim.cmd("set spelllang=en,de")
 
 return {
+  -- none lua plugins that I still use
+  { "easymotion/vim-easymotion" },
+  { "terryma/vim-multiple-cursors" },
+  { "tpope/vim-fugitive" },
+  { "tpope/vim-eunuch" },
+
   -- colorschemes
   { "tanvirtin/monokai.nvim", opts = {} },
-  { "easymotion/vim-easymotion" },
-  { "tpope/vim-fugitive" },
-  { "terryma/vim-multiple-cursors" },
-  { "tpope/vim-eunuch" },
   {
     "LazyVim/LazyVim",
     opts = {
       colorscheme = "monokai",
     },
   },
-  {},
 }
