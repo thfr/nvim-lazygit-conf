024300f9fedabc7a80d1954cc074e145a4cdb693
thfr <thfr.git@e.mail.de>
2025-05-10 23:35:21 +0200

    4 spaces 1 tab

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,3 +2,5 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.g.snacks_animate = false
+vim.opt.shiftwidth = 4 -- Number of spaces for an indentation
+vim.opt.tabstop = 4 -- Number of spaces tabs count for
